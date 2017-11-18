63df44d25bc13b1e07d8c22dca750f8c873358ae
Meaningisuse <meaningisusechiu@gmail.com>
2017-11-19 00:04:20 +0800

    Update opkg-install.sh

--- opkg-install.sh
+++ opkg-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-opkg install luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn adblock luci-app-adblock luci-i18n-adblock-zh-cn luci-theme-material
+opkg install luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn adblock luci-app-adblock luci-i18n-adblock-zh-cn luci-theme-material wget
