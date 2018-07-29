c133d0c3d1e62ff8ba76561fb2bceaa3fa357b22
Meaningisuse Chiu <meaningisusechiu@gmail.com>
2018-07-30 00:53:56 +0800

    update

--- OpenSUSE_Tumbleweed.sh
+++ OpenSUSE_Tumbleweed.sh
@@ -2,12 +2,15 @@
 #rpm --import linux_signing_key.pub
 #rm linux_signing_key.pub
 #zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
+#zypper addrepo --refresh http://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed/ snappy
 #wget https://raw.githubusercontent.com/ugetdm/uget-integrator/master/install/linux/install_uget_integrator.sh
 #chmod +x install_uget_integrator.sh
 #./install_uget_integrator.sh
 zypper refresh
-zypper install google-chrome-stable broadcom-wl unetbootin gnome-keyring texlive git nodejs8 npm8 cmake fcitx fcitx-googlepinyin transmission-qt uget asciidoc fastjar flex gcc gcc-c++ intltool jikes zlib-devel-static mercurial ncurses-devel libopenssl-devel patch perl-ExtUtils-MakeMaker python-devel sdcc gettext-tools
+zypper dup
+#zypper install google-chrome-stable broadcom-wl unetbootin gnome-keyring texlive git nodejs8 npm8 cmake fcitx fcitx-googlepinyin transmission-qt uget asciidoc fastjar flex gcc gcc-c++ intltool jikes zlib-devel-static mercurial ncurses-devel libopenssl-devel patch perl-ExtUtils-MakeMaker python-devel sdcc gettext-tools snapd
 #ln -s /usr/lib64/libcurl.so.4 /usr/lib64/libcurl-gnutls.so.4
+#systemctl enable --now snapd.socket
 snap install --classic pycharm-community
 snap install --classic vscode 
 snap install --classic android-studio
