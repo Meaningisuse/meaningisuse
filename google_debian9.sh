6ba7d259f747f495eab8eaec04aa53fc66124e57
QiuCanHao <qch@insucrm.com>
2019-05-27 17:55:33 +0800

    新增google_debian脚本

---
+++ google_debian9.sh
@@ -0,0 +1,15 @@
+modprobe tcp_bbr
+echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+apt update
+apt install -y bash-completion apt-transport-https ca-certificates curl gnupg2 software-properties-common htop
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
