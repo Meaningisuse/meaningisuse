afb09afe5bf814d59e60d36060dcecd406980e53
QiuCanHao <qch@insucrm.com>
2019-07-03 11:19:11 +0800

    Update google_debian9.sh

--- google_debian9.sh
+++ google_debian9.sh
@@ -5,6 +5,7 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 
 apt update
+apt upgrade -y
 apt install -y bash-completion apt-transport-https ca-certificates curl gnupg2 software-properties-common htop
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
