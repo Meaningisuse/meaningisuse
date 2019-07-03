b1749ac7e2d318954e1cea6443b0547a511bd306
QiuCanHao <qch@insucrm.com>
2019-07-03 10:54:00 +0800

    Update debian_shadowsocksr.sh

--- debian_shadowsocksr.sh
+++ debian_shadowsocksr.sh
@@ -1,12 +1,3 @@
-apt update 
-apt upgrade -y
-apt install -y htop
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-modprobe tcp_bbr
-echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-reboot
